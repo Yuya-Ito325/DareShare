855b310aa376bdb71cbfabd7b71cf678cc296ba0
Yuya-Ito325 <c1004072@planet.kanazawa-it.ac.jp>
2021-12-28 22:35:32 +0900

    first commit

---
+++ DateShare/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DateShare
+//
+//  Created by 伊藤祐哉 on 2021/12/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
