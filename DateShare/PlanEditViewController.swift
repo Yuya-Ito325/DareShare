62961737ae3ea7642c2c54241e9dbbafbbc22aa4
HappyMana <c1002140@planet.kanazawa-it.ac.jp>
2021-12-29 00:28:52 +0900

    Issue-3 必要な画面数のstoryboardとswiftファイルを作った-mana

---
+++ DateShare/PlanEditViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PlanEditViewController.swift
+//  DateShare
+//
+//  Created by 木下真菜 on 2021/12/29.
+//
+
+import UIKit
+
+class PlanEditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
